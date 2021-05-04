774226baf0438ceaf05ff3fb143f46bc31adc2a1
bit-shashank <shashankcs083@gmail.com>
2021-05-04 23:35:57 +0530

    Initial Commit

---
+++ client/V-Meet-Client/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed=4;
+    public float gravity=10.0f;
+    Vector3 moveDir=Vector3.zero;
+    CharacterController controller;
+	public float jumpSpeed=20.0f;
+
+    void Start(){
+        controller=GetComponent<CharacterController>();
+    }
+
+    void Update(){
+        if(controller.isGrounded){	
+            float x=Input.GetAxis("Horizontal");
+            float z=Input.GetAxis("Vertical");
+            moveDir=transform.right*x +transform.forward*z;
+            moveDir=moveDir*speed;
+            if(Input.GetButtonDown("Jump")){
+					moveDir.y=jumpSpeed;
+			}
+        }
+		moveDir.y-=gravity*Time.deltaTime;
+		controller.Move(moveDir*Time.deltaTime); 
+    }
+
+}
---
+++ client/V-Meet-Client/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour {
+
+	public float  mouseSensitivity=200.0f;
+
+	public Transform playerBody;
+
+	float xRotation=0.0f;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState=CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mouseX=Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
+		float mouseY=Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
+
+		xRotation-=mouseY;
+		xRotation=Mathf.Clamp(xRotation,20.0f,30.0f);
+		transform.localRotation= Quaternion.Euler(xRotation,0.0f,0.0f);
+		playerBody.Rotate(Vector3.up*mouseX);
+	}
+}
